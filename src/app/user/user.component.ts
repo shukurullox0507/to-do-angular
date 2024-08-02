b3d002a1ce8bb2eec0e3605c68b5719c7004fa6d
shukurullox <shukurulloxdeveloper@gmail.com>
2024-08-02 09:26:28 +0500

    finish codes

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
+
+// type User = { id: string; avatar: string; name: string };
+
+// interface User {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected! : boolean;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>()
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // })
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
